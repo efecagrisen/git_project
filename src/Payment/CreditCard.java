95dc2e64f56407725baf9e4d98ad5e61415dca2e
efecagrisen <efecagrisen@yahoo.com>
2023-07-10 13:34:46 +0300

    CreditCard class completed

--- src/Payment/CreditCard.java
+++ src/Payment/CreditCard.java
@@ -4,5 +4,7 @@ public class CreditCard {
     public static void main(String[] args) {
         System.out.println("main method added and committed");
 
+        System.out.println("class completed");
+
     }
 }
